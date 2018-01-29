1550b2f9c8814bb55487fcd4852f5ac83fdf59c4
Konailky <32402567+Konailky@users.noreply.github.com>
2018-01-29 14:38:02 -0800

    1.29.18
    finished bubble sort

--- MoreArrays/Sort.java
+++ MoreArrays/Sort.java
@@ -9,46 +9,49 @@
 public class Sort
 {
     int[] array = {5,6,7,2,1,3,8,4};
+    boolean check = false;
 
-    public Sort(){//Runs everything and checks to see if the array is sorted or not.
-
-        bubbleSort();
-        //insertSort();
-        //selectSort();
-
-        boolean check = false;
-
-        while ( check == false){
-            for ( int i = 0; i < array.length-1; i++){
-                if (array[i] > array[i+1]){
-                    check = false;
-                }
-                i++;
+    public Sort(){//Runs everything.
+        while(check == false){
+            bubbleSort();
+            //insetSort();
+            //selectSort();
+            if (check()){
+                check = true;
             }
-            check = true;
         }
     }
 
-    public  void bubbleSort(){
+    public boolean check(){
+        check = true;
+        for (int i = 0; i < array.length-1; i++){
+            if ( array[i] > array[i+1]){
+                check = false;
+            }
+        }
+        return check;
+    }
 
+    public void bubbleSort(){
         for (int i = 0; i < array.length-1; i++){
             if(array[i] > array[i+1]){
-                int temp = array[i+1];
-                array[i+1] = array[i];
-                array[i] = temp;
+                int temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
             }
-            i++;
-        }
-        System.out.print("BubbleSort:");
-         System.out.println(" ");
-        for(int i : array){//Printing the sorted list array
-            System.out.print(i);
         }
     }
 
     public  void insertSort(){
+        int max = array[array.length-1];
+       // for (int i = array.length-1; i >0 ; i--){
+        
+        
+     
     }
+
 
     public  void selectSort(){
     }
 }
+
